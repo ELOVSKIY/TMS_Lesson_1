99fbd3c756117716924394c49c3ef7d4d321fc24
Stas Elovsky <42416774+ELOVSKIY@users.noreply.github.com>
2023-12-13 20:55:42 +0200

    Add files via upload

---
+++ Lesson_1/src/Main.java
@@ -0,0 +1,75 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello TMS!");
+        //System.out.println("Hello World!");
+        // System.out.println("Hello Minsk!");
+
+        //
+//        String str = "Hello TMS!"; // объявление переменной и инициализация
+//        System.out.println(str);
+//        str = "Hello world"; // изменение значения переменной
+//        System.out.println(str);
+//
+//        String someStr = "Hello Minsk"; // объявление  второй переменной и инициализация
+//        System.out.println(someStr);
+
+//        int number = 5;
+//        System.out.println(number);
+//        number = 6;
+//        System.out.println(number);
+//        number = 5 + 10;
+//        System.out.println(number);
+//        number = 5 * 10;
+//        System.out.println(number);
+//        number = -5;
+//        System.out.println(number);
+//        // можно писать значения от -2млрд до 2 млрд
+//        number = 8 / 3;
+//        System.out.println(number);
+
+//        int numb = 5;
+////        numb = "Sre" // не скопмилируется
+//        int number = 6;
+//        System.out.println(number);
+////        System.out.println(numb * number);
+//        number = number + numb;
+//        System.out.println(number);
+//        number = 6;
+
+//        byte b = 120; // от -128 до 127 - это важно, если к 127 прибавить 1 то будет -128
+//        // размер в памяти занимает 1 байт
+//        // 2^8 - 256
+//        // 00000001 в двоичной - 1 в десятиричной
+//        System.out.println(b);
+//
+//        short s = 129; // 2 байта и знаения от -32 тысяч до 32 тысяч
+//        // 00000000 00000001
+//        // 2^16
+//        System.out.println(s);
+//
+//        int i = 432423040; // 4 байта и знаения от -2 млрд до 2 млрд
+//        // 00000000 00000000 00000000 00000001
+//        // 2^32
+//        System.out.println(i);
+//
+//        long l = 20_000_000_000l * 50; //
+//        // 2^64
+//        System.out.println(l);
+
+
+        float f = 5.5F;
+        System.out.println(f);
+        f = 8F;
+        System.out.println(f);
+        f = f / 3;
+        System.out.println(f);
+
+
+        double d = 5.5f;
+        System.out.println(d);
+        d = 8F;
+        System.out.println(d);
+        d = d / 3;
+        System.out.println(d);
+    }
+}
---
+++ Lesson_1/src/scanner/Main.java
@@ -0,0 +1,20 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//        String name = scanner.nextLine();
+//        System.out.println("Hello " + name);
+
+//        String name1 = scanner.next();
+//        System.out.println(name1);
+//
+//        String name2 = scanner.next();
+//        System.out.println(name2);
+
+        int a =scanner.nextInt();
+     }
+}
